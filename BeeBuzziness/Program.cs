50c8631a640aea11f656cf1d40e06a9554b8187e
Elsa <elsa.demaine@epsi.fr>
2018-08-08 15:12:55 +0200

    [add] security && [add] boucle

--- BeeBuzziness/Program.cs
+++ BeeBuzziness/Program.cs
@@ -10,12 +10,23 @@ namespace BeeBuzziness
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Veuillez saisir un nombre.");
-            string nombre = Console.ReadLine();
-
             AffichageLCD lcd = new AffichageLCD();
-            lcd.Print(nombre);
+            bool exit = true;
 
+            while (exit) { 
+                Console.WriteLine("Veuillez saisir un nombre.");
+                string nombre = Console.ReadLine();
+                bool integer = int.TryParse(nombre, out int nb);
+                if (integer)
+                    lcd.Print(nombre);
+                else
+                    Console.WriteLine("Erreur. Veuillez saisir que des nombre. (ex : 0123)");
+                Console.WriteLine("Voulliez vous quitter ? (o/n)");
+                string quit = Console.ReadLine();
+                if (quit == "o" || quit == "O")
+                    exit = false;
+                Console.Clear();
+            }
         }        
     }
 }
